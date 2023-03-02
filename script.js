2e0773122a09e399fc500fa3a0509d2d17a6eadb
Tamas <tamas.fehertoi@gmail.com>
2023-03-02 16:20:26 +0100

    event-listener

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
-console.log("hello")
+console.log('hello')
 
+const buttonElement = document.querySelector('.click-button')
+const bodyElement = document.querySelector('body')
+const rootElement = document.querySelector('#root')
+
+buttonElement.innerHTML = "klikkelj rám"
+
+let darkTheme = false
+
+buttonElement.addEventListener('click', function () {
+  console.log('clicked')
+
+  darkTheme = !darkTheme
+  
+  if (darkTheme === true) {
+    buttonElement.innerHTML = "dark theme is on"
+    bodyElement.classList.add('dark')
+  } else {
+    buttonElement.innerHTML = "dark theme is off"
+    bodyElement.classList.remove('dark')
+  }
+})
+
+setTimeout(function () {
+  rootElement.insertAdjacentHTML('beforeend' ,`<p>hello world</p>`)
+
+  console.log('hello world')
+}, 6000)
